abec2849b0eacc6d543e3a5da234334981f1f9f1
lemats <>
2011-07-27 17:27:57 +0200

    Add lang de

---
+++ Phorm/lang/de.php
@@ -0,0 +1,22 @@
+<?php if(!defined('PHORMS_ROOT')) { die('Phorms not loaded properly'); }
+
+$lang['field_file_toolarge'] = 'Das File war zu gross.';
+$lang['field_file_uploaderror'] = 'Während dem File-Upload ist ein Fehler aufgetreten. Bitte nochmals versuchen.';
+$lang['field_file_notsent'] = 'Das File wurde nicht gesendet. Bitte nochmals versuchen.';
+$lang['field_file_syserror'] = 'Während dem Upload ist ein Fehler aufgetreten. Bitte kontaktieren Sie den Webmaster (Fehler Nr. %s).';
+$lang['field_file_badtype'] = 'File-Type %s ist nicht zulässig.';
+$lang['field_file_sizelimit'] = 'File-Grösse ist limitiert auf %s bytes.';
+$lang['field_invalid_text_sizelimit'] = 'Text ist auf max. %s Zeichen limitiert.';
+$lang['field_invalid_integer'] = 'Muss eine Ganzzahl sein.';
+$lang['field_invalid_integer_sizelimit'] = 'Muss eine Ganzzahl mit weniger als %s Ziffern sein.';
+$lang['field_invalid_alpha'] = 'Darf nur Buchstaben enthalten.';
+$lang['field_invalid_alphanum'] = 'Darf nur alphanumerische Zeichen enthalten.';
+$lang['field_invalid_decimal'] = 'Ungültiger Dezimalwert.';
+$lang['field_invalid_dropdown'] = 'Auswahl ist ungültig.';
+$lang['field_invalid_url'] = 'URL ist ungültig.';
+$lang['field_invalid_email'] = 'E-Mailadresse ist ungültig.';
+$lang['field_invalid_datetime_format'] = 'Datums- oder Zeitformat ist ungültig.';
+$lang['field_invalid_multiplechoice_badformat'] = 'Auswahl ist ungültig.';
+$lang['buttons_validate'] = 'Validiere';
+$lang['buttons_reset'] = 'Formular zurücksetzen';
+$lang['validation_required'] = 'Dieses Feld ist ein Pflichtfeld.';
