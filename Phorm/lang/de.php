d8ef4728c0e643f210dd28b8158fa00c6d694f86
lemats <>
2011-07-27 18:10:47 +0200

    Replace File by Datei

--- Phorm/lang/de.php
+++ Phorm/lang/de.php
@@ -1,11 +1,11 @@
 <?php if(!defined('PHORMS_ROOT')) { die('Phorms not loaded properly'); }
 
-$lang['field_file_toolarge'] = 'Das File war zu gross.';
-$lang['field_file_uploaderror'] = 'Während dem File-Upload ist ein Fehler aufgetreten. Bitte nochmals versuchen.';
-$lang['field_file_notsent'] = 'Das File wurde nicht gesendet. Bitte nochmals versuchen.';
+$lang['field_file_toolarge'] = 'Die Datei war zu gross.';
+$lang['field_file_uploaderror'] = 'Während dem Datei-Upload ist ein Fehler aufgetreten. Bitte nochmals versuchen.';
+$lang['field_file_notsent'] = 'Die Datei wurde nicht gesendet. Bitte nochmals versuchen.';
 $lang['field_file_syserror'] = 'Während dem Upload ist ein Fehler aufgetreten. Bitte kontaktieren Sie den Webmaster (Fehler Nr. %s).';
-$lang['field_file_badtype'] = 'File-Type %s ist nicht zulässig.';
-$lang['field_file_sizelimit'] = 'File-Grösse ist limitiert auf %s bytes.';
+$lang['field_file_badtype'] = 'Datei-Type %s ist nicht zulässig.';
+$lang['field_file_sizelimit'] = 'Datei-Grösse ist limitiert auf %s bytes.';
 $lang['field_invalid_text_sizelimit'] = 'Text ist auf max. %s Zeichen limitiert.';
 $lang['field_invalid_integer'] = 'Muss eine Ganzzahl sein.';
 $lang['field_invalid_integer_sizelimit'] = 'Muss eine Ganzzahl mit weniger als %s Ziffern sein.';
